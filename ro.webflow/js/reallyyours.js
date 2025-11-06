c66319ec37b7d762f1390d04ba3728c17ce74b11
Zak Khaliq <zkhalique@gmail.com>
2025-11-05 16:43:49 -0800

    changed dir name and some file names

---
+++ ro.webflow/js/reallyyours.js
@@ -0,0 +1,67 @@
+(() => {
+    $(function() {
+
+        const gifs = document.querySelectorAll('.gif');
+        gifs.forEach(gif => {
+            gif.addEventListener('load', function() {
+            this.style.display = 'block';
+            this.previousElementSibling.style.display = 'none';
+            });
+        });
+
+        //content selector buttons
+        var contentSelected = false; // Set this to true or false based on your condition
+
+        document.getElementById('toggleButtonGroup').addEventListener('click', function(event) {
+          
+          // Ensure that one of the buttons was clicked
+          if (event.target.id == "community") {
+            $("#benefits-creator").css("display", "none");
+            $("#features-creator").css("display", "none");
+            $("#pricing-creator").css("display", "none");
+
+            $("#benefits-community").css("display", "block");
+            $("#features-community").css("display", "block");
+            $("#pricing-community").css("display", "block");
+
+          }
+
+          if (event.target.classList.contains('toggle-button')) {
+              
+              // Get all buttons
+              var buttons = document.querySelectorAll('.toggle-button');
+              
+              // Reset all buttons to inactive state
+              buttons.forEach(function(button) {
+                  button.classList.remove('active');
+              });
+      
+              // Set the clicked button to active state
+              event.target.classList.add('active');
+          }
+          contentSelected = true;
+          $("#main-content-select").css("display", "none");
+          $("#main-content-container").fadeIn(1000);  
+          $('html, body').animate({
+            scrollTop: $('#benefits').offset().top
+          }, 1000);  // 1000 is the duration in milliseconds
+      
+        });
+      
+        //scroll user to main selector buttons if they click the nav menu before selecting who they are
+        $('a.navlink, a.blue-fill-button').click(function(e){
+            if (!contentSelected) {
+                e.preventDefault(); // Prevent default anchor behavior
+                var target = $("#main-content-select");
+    
+                if(target.length){
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000); // Duration of scroll
+                }
+            }
+            // If contentSelected is true, normal anchor behavior will occur
+        });
+    });
+})();
+  
