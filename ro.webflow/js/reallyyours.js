b9af5b9cb200bfefb9a0149541ba0c877503ff45
Zak Khaliq <zkhalique@gmail.com>
2025-12-12 20:19:31 -0800

    added scroll up when read less is clicked

--- ro.webflow/js/reallyyours.js
+++ ro.webflow/js/reallyyours.js
@@ -11,6 +11,11 @@
                 $text.removeClass('expanded').addClass('collapsed');
                 $button.attr('aria-expanded', 'false');
                 $button.find('.toggle-text').text('Read more');
+                // Scroll to top of manifesto container
+                const $container = $('.manifesto-container');
+                $('html, body').animate({
+                    scrollTop: $container.offset().top - 100 // 100px offset from top
+                }, 500);
             } else {
                 $text.removeClass('collapsed').addClass('expanded');
                 $button.attr('aria-expanded', 'true');
